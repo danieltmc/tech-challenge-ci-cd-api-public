3148aba8a2b63a2fa1a005cf3593f187577d269d
Daniel McCloskey <me@danieltmc.com>
2023-08-25 15:41:45 -0400

    Add minimal Spring API

---
+++ src/main/java/com/myacuity/techchallengecicdapi/TechChallengeCiCdApiApplication.java
@@ -0,0 +1,13 @@
+package com.myacuity.techchallengecicdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeCiCdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeCiCdApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myacuity/techchallengecicdapi/TechChallengeCiCdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myacuity.techchallengecicdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeCiCdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
