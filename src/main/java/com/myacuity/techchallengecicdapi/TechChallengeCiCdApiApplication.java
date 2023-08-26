b657909b130d5cfcd168e6e3e801de9385259408
Daniel McCloskey <me@danieltmc.com>
2023-08-26 14:03:44 -0400

    Print Swagger URL on startup

--- src/main/java/com/myacuity/techchallengecicdapi/TechChallengeCiCdApiApplication.java
+++ src/main/java/com/myacuity/techchallengecicdapi/TechChallengeCiCdApiApplication.java
@@ -1,13 +1,21 @@
 package com.myacuity.techchallengecicdapi;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class TechChallengeCiCdApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechChallengeCiCdApiApplication.class, args);
+		log.info("""
+
+				-------------------------------------------------------
+				\tSwagger UI: http://localhost:8080/swagger-ui/index.html
+				-------------------------------------------------------
+				""");
 	}
 
 }
